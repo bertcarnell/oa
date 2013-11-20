85dc32136209cae0da76fd826f533b8e15372954
rc <bertcarnell@gmail.com>
2013-11-20 14:47:44 -0500

    Updated batch scripts to convert to new package structure

--- oa_nb/deployoa.bat
+++ oa_nb/deployoa.bat
@@ -1,26 +1,30 @@
-
 echo off
 
+:: Rtools uses CYGWIN for these file operations
 set CYGWIN=nodosfilewarning
+:: Deploy to lhs repository at r-forge.r-project.org
+:: change this destination to match your path
+set DESTINATION=C:\Users\carnellr\Documents\Repositories\lhs\admin\pkg\oalhs\src
+:: dos2unix
+set DOS2UNIX="C:\MinGW\msys\1.0\bin\dos2unix.exe"
 
-:: copy oa library from oalib and oa_r to oa
+:: copy oa library from oalib and oa_r to oalhs
 echo ****
 echo **** copying sources ****
 echo ****
-cp ./oalib/*.h ./oa/src
-cp ./oalib/*.cpp ./oa/src
-cp ./oa_r/*.h ./oa/src
-cp ./oa_r/*.cpp ./oa/src
+cp ./oalib/*.h %DESTINATION%
+cp ./oalib/*.cpp %DESTINATION%
+cp ./oa_r/*.h %DESTINATION%
+cp ./oa_r/*.cpp %DESTINATION%
 
 :: ensure only unix line endings
 ::"C:\MinGW\msys\1.0\bin\dos2unix.exe" ./oa/src/*.*
 echo ****
 echo **** dos2unix ****
 echo ****
-for /r "./oa/src" %%x in (*.cpp) do "C:\MinGW\msys\1.0\bin\dos2unix.exe" "%%x"
-for /r "./oa/src" %%x in (*.h) do "C:\MinGW\msys\1.0\bin\dos2unix.exe" "%%x"
+for /r "%DESTINATION%" %%x in (*.cpp) do %DOS2UNIX% "%%x"
+for /r "%DESTINATION%" %%x in (*.h) do %DOS2UNIX% "%%x"
 
 echo on
 
 PAUSE
-
--- oa_nb/documentWithDoxygen.bat
+++ oa_nb/documentWithDoxygen.bat
@@ -1,4 +1,11 @@
-:: to generate the config file the first time (with defaults)
-:: "c:\Program Files\doxygen\bin\doxygen.exe" -g doxygen.config
+:: document code with 
 
-"c:\Program Files\doxygen\bin\doxygen.exe" doxygen.config
+echo off
+
+set DOXYGEN="c:\Program Files\doxygen\bin\doxygen.exe"
+set CONFIGFILE=doxygen.config
+
+:: to generate the config file the first time (with defaults)
+:: %DOXYGEN% -g %CONFIGFILE%
+
+%DOXYGEN% %CONFIGFILE%
