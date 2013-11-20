ef433b0dcaaec43302fa2d5e7570a05595cfebe2
rc <bertcarnell@gmail.com>
2013-11-19 22:38:15 -0500

    Removed boost deployment from the deployoa.bat code

--- oa_nb/deployoa.bat
+++ oa_nb/deployoa.bat
@@ -12,12 +12,6 @@ cp ./oalib/*.cpp ./oa/src
 cp ./oa_r/*.h ./oa/src
 cp ./oa_r/*.cpp ./oa/src
 
-:: copy boost files using bcp to a boost directory
-echo ****
-echo **** copying boost files using bcp to a boost directory ****
-echo ****
-"c:\Program Files\boost\boost_1_51\bin\bcp.exe" --scan --boost="..\..\boost" .\oa\src\CommonDefines.h .\oa\src > boostcopy.log
-
 :: ensure only unix line endings
 ::"C:\MinGW\msys\1.0\bin\dos2unix.exe" ./oa/src/*.*
 echo ****
