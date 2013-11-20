24c5f5474c07ae689923d63d652c6d2430adb5a2
rc <bertcarnell@gmail.com>
2013-11-19 22:38:38 -0500

    Added comments to R CMD check

--- oa_nb/rcmdcheckoa.bat
+++ oa_nb/rcmdcheckoa.bat
@@ -1,11 +1,23 @@
 :: Check the R build
 
+echo off
 echo *******
 echo This check must be run as an administrator
 echo *******
 
+set CYGWIN=nodosfilewarning
+
+echo ****
+echo **** removing old checks ****
+echo ****
 rm -rf .\oa\src-i386
 rm -rf .\oa\src-x64
+
+echo ****
+echo **** R CMD check oa ****
+echo **** 
 "c:\Program Files\R\R-3.0.1\bin\R.exe" CMD check oa
+
+echo on
 
 PAUSE
